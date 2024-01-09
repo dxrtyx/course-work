eba66ec365ae92547f21a11c18da6bf3657f6e71
dxrtyx <dartcomand@gmail.com>
2024-01-09 15:15:42 +0200

    scss framework manual

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,6 @@ import * as myFunctions from './modules/functions.js';
 //import { Navigation, Pagination } from 'swiper/modules';
 
 import modal from './modal/modal.js';
-//import send_message from './modal/send_message.js';
+import mailer from './modal/mailer.js';
 
 import help_us__swiper from './swiper/help_us__swiper.js';
